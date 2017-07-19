4997d2d10e8c83211c460985f8f182f69a16c449
Logan Wood <lwood3499@gmail.com>
2017-07-19 18:25:20 -0500

    additional basic language practice

---
+++ practice/LuckySevens/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LuckySevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                {
+                    totalNumberOfSevens++;
+                }
+            }
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens!", totalNumberOfSevens);
+        }
+    }
+}
--- practice/mathproblems/Program.cs
+++ practice/mathproblems/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace mathproblems
+namespace GuessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int playerGuess;
+            string playerInput;
+            bool isNumberGuessed = false;
+            int theAnswer;
+
+            Random rng = new Random();
+            theAnswer  = rng.Next(1, 21);
+
+            do
+            {
+                Console.Write("Enter your guess: ");
+                playerInput = Console.ReadLine();
+
+                playerGuess = int.Parse(playerInput);
+
+                if (playerGuess == theAnswer)
+                {
+                    Console.WriteLine("You got it!");
+                    isNumberGuessed = true;
+                }
+                else
+                {
+                    if (playerGuess > theAnswer)
+                    {
+                        Console.WriteLine("Too high!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too low!");
+                    }
+                }
+            } while (!isNumberGuessed);
         }
     }
 }
